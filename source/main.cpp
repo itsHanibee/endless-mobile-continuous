9b83d03e32c53a741618fad22d7633a5d7043355
TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>
2024-07-04 21:32:58 +0200

    feat(enhancement): `<model>` replacement for model name of the flagship (#10232)

--- source/main.cpp
+++ source/main.cpp
@@ -680,6 +680,7 @@ Conversation LoadConversation()
 		{"<passengers>", "[your passengers]"},
 		{"<planet>", "[Planet]"},
 		{"<ship>", "[Ship]"},
+		{"<model>", "[Ship Model]"},
 		{"<system>", "[Star]"},
 		{"<tons>", "[N tons]"}
 	};
