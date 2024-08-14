f1cd351bbbdcb324bbebd67bd9e8813a3d4451a6
Unordered Sigh <116329264+UnorderedSigh@users.noreply.github.com>
2024-08-14 07:25:34 +0000

    Expand `${phrases}` and `<substitutions>` in NPC and gift ship names (#8854)

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -2337,7 +2337,14 @@ map<string, string> PlayerInfo::GetSubstitutions() const
 {
 	map<string, string> subs;
 	GameData::GetTextReplacements().Substitutions(subs, Conditions());
+	AddPlayerSubstitutions(subs);
+	return subs;
+}
 
+
+
+void PlayerInfo::AddPlayerSubstitutions(map<string, string> &subs) const
+{
 	subs["<first>"] = FirstName();
 	subs["<last>"] = LastName();
 	const Ship *flag = Flagship();
@@ -2350,7 +2357,6 @@ map<string, string> PlayerInfo::GetSubstitutions() const
 	subs["<system>"] = GetSystem()->Name();
 	subs["<date>"] = GetDate().ToString();
 	subs["<day>"] = GetDate().LongString();
-	return subs;
 }
 
 
